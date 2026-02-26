07b3b7126c233f84058870d36a8adf6a827699c3
Anshuman <anshumaansingh98@gmail.com>
2026-02-26 17:27:40 +0530

    Deploy field-addition: CHG-901

--- UPISim/src/main/java/com/npci/UPISim/validation/ValidationRules.java
+++ UPISim/src/main/java/com/npci/UPISim/validation/ValidationRules.java
@@ -18,4 +18,7 @@ public final class ValidationRules {
 
     public static final Set<String> REQPAY_TXN_DELEGATE_ALLOWED_VALUES =
             Set.of("Y", "N");
+
+    public static final Set<String> REQPAY_TXN_DELEGATE_ALLOWED_VALUES =
+            Set.of("Y", "N");
 }

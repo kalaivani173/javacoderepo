c2f0c3fea120bac60900a06cc6c5c7a85888fa19
Kalai <kalaisanu@gmail.com>
2026-03-12 23:50:48 +0530

    Deploy field-addition: CHG-965

--- UPISim/src/main/java/com/npci/UPISim/dto/Device_old.java
+++ UPISim/src/main/java/com/npci/UPISim/dto/Device_old.java
@@ -9,8 +9,13 @@ public class Device {
     @XmlElement(name = "Tag")
     private List<Tag> tags;
 
+    @XmlAttribute(name = "BINDINGMODE")
+    private String bindingMode;
+
     // getters and setters
     public List<Tag> getTags() { return tags; }
     public void setTags(List<Tag> tags) { this.tags = tags; }
+    
+    public String getBindingMode() { return bindingMode; }
+    public void setBindingMode(String bindingMode) { this.bindingMode = bindingMode; }
 }
-
